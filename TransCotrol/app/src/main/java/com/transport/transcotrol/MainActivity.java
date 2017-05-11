a53045a880083e2e1f20d751ec6cdf9b63204341
Afraoo <moloop@outlook.com>
2017-05-11 17:01:34 +0800

    布局的基本实现
    还差：
    1.下拉菜单的动态加载；
    2.按钮的响应事件

--- TransCotrol/app/src/main/java/com/transport/transcotrol/MainActivity.java
+++ TransCotrol/app/src/main/java/com/transport/transcotrol/MainActivity.java
@@ -2,12 +2,20 @@ package com.transport.transcotrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner spinner = null;
+    private ArrayAdapter<CharSequence> adapter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//setContentView();显示在MainActivity这个布局文件中要显示的内容,即activity_main.xml中的内容
+
+        //spinner = (Spinner) findViewById(R.id.spinner1);
+       // adapter = new ArrayAdapter<CharSequence>(this,R.layout.simple_spinner_item,data_list);
+        //spinner.setAdapter(adapter);
     }
 }
